0de4a9eda31c8d9f0dad34ef6ed2b616f799527b
pablodcb7 <pablodcb7@gmail.com>
2024-10-10 18:02:57 +0200

    prueba

---
+++ src/Alumno.java
@@ -0,0 +1,38 @@
+public class Alumno {
+    private String dni;
+    private String nombreCompleto;
+    private String fechaNac;
+    private String direccion;
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getNombreCompleto() {
+        return nombreCompleto;
+    }
+
+    public void setNombreCompleto(String nombreCompleto) {
+        this.nombreCompleto = nombreCompleto;
+    }
+
+    public String getFechaNac() {
+        return fechaNac;
+    }
+
+    public void setFechaNac(String fechaNac) {
+        this.fechaNac = fechaNac;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+}
