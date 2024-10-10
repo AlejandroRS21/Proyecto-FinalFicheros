d06081b7d5c52cacd986a11391f0e537015480ef
pablodcb7 <pablodcb7@gmail.com>
2024-10-10 18:08:49 +0200

    prueba

---
+++ src/Asignatura.java
@@ -0,0 +1,21 @@
+public class Asignatura {
+    //Atributos
+    private int codAsignatura;
+    private String nombreAsignatura;
+
+    public int getCodAsignatura() {
+        return codAsignatura;
+    }
+
+    public void setCodAsignatura(int codAsignatura) {
+        this.codAsignatura = codAsignatura;
+    }
+
+    public String getNombreAsignatura() {
+        return nombreAsignatura;
+    }
+
+    public void setNombreAsignatura(String nombreAsignatura) {
+        this.nombreAsignatura = nombreAsignatura;
+    }
+}
