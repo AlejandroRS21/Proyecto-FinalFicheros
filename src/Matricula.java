7cb0e318dea67c999aa1cc2f23305965c637de76
pablodcb7 <pablodcb7@gmail.com>
2024-10-10 18:05:29 +0200

    prueba

---
+++ src/Matricula.java
@@ -0,0 +1,29 @@
+public class Matricula {
+    private int codMatricula;
+    private String dni;
+    private int codAsignatura;
+
+    public int getCodMatricula() {
+        return codMatricula;
+    }
+
+    public void setCodMatricula(int codMatricula) {
+        this.codMatricula = codMatricula;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public int getCodAsignatura() {
+        return codAsignatura;
+    }
+
+    public void setCodAsignatura(int codAsignatura) {
+        this.codAsignatura = codAsignatura;
+    }
+}
