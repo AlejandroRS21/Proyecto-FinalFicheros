1bbe239ac5c31c8b6636f126f99d103d72a8b26f
Hugo <hugo.hees2005@gmail.com>
2024-10-10 18:10:15 +0200

    Prueba1

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,20 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner (System.in);
+        Scanner sc = new Scanner(System.in);
         int opcion;
 
-        do{
+        do {
             System.out.print("MENU");
-            System.out.print("\n1.Introduce nuevo alumno");
-            System.out.print("\n2.Mostrar informacion en funcion de una nota");
-            System.out.print("\n3.Salir\n");
-            opcion=sc.nextInt();
-
-
-            switch(opcion){
+            System.out.print("\n1.Introduce alumno");
+            System.out.print("\n2.Introduce matricula");
+            System.out.print("\n3.Introduce asignatura");
+            System.out.print("\n4.Mostrar informacion alumno");
+            System.out.print("\n5.Volcar todos los alumnos a fichero.");
+            System.out.print("\n6.Borrar todo\n");
+            opcion = sc.nextInt();
+            switch (opcion) {
                 case 1:
                     introducir();
                     break;
@@ -24,8 +27,7 @@ public class Main {
                 default:
                     System.out.print("Numero incorrecto vuelva a escribir uno");
             }
-        }while(opcion != 3);
+        } while (opcion != 3);
         sc.close();
     }
 }
-//soy hugo
